9cce886c1653a3136f16fd77312ca422a76d6f6f
evannatvig <natvigevan@gmail.com>
2022-04-06 08:00:19 -0500

    SQLite to Postgres, update environment variables.

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('API_SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('API_SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
